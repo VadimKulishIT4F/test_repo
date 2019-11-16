521f21dbaf70a3a9f78c9386c0ab82c0c5f2f515
VadimKulishIT4F <57797583+VadimKulishIT4F@users.noreply.github.com>
2019-11-16 22:59:35 +0300

    Update H1 Step one.sql

--- H1 Step one.sql
+++ H1 Step one.sql
@@ -1,9 +1,9 @@
 -- Видим что данные в экселе, поэтому для импорта сохраняем  файлы в формате CSV (файл/сохранить как)
---(Для преподавателя - при импорте данных из файла bond_description_task была ошибка в столбцах HaveOffer, AmortisedMty и IsConvertible. 
+--(при импорте данных из файла bond_description_task была ошибка в столбцах HaveOffer, AmortisedMty и IsConvertible. 
 --по одному эмитенту данные в этих столбцах были в форме даты, а не boolean как в остальных строках,
---по умолчанию проставлены вручную нулевые значения).
+--по умолчанию оставил клетки пустыми).
 
--- Создаем первую таблицу bond_description_task  с нужными форматом и нужным количеством полей.
+-- Создаем первую таблицу bond_description_task  с нужными форматом и нужным количеством полей, предварительно удаляем старую версию таблицы
 DROP TABLE if exists public.bond_description_task;
 CREATE TABLE public.bond_description_task
 (
@@ -39,8 +39,67 @@ WITH (
 )
 TABLESPACE pg_default;
 
+-- команда назначает владельца таблицы 
+
+ALTER TABLE public.bond_description_task
+    OWNER to postgres;
+
 
 --Команда импортирует данные в созданную таблицу. Выполнять через терминал  SQL Shell,
 --так как в другом способе могут возникнуть проблемы с доступом к файлу
 
 \copy public.bond_description_task FROM 'C:/data/bond_description_task.csv' DELIMITER ';' CSV HEADER ENCODING 'WIN 1251';
+
+
+--  Создаем таблицу quotes_task  с нужными форматом и нужным количеством полей, предварительно удаляем старую версию таблицы.
+
+DROP TABLE if exists public.quotes_task;
+CREATE TABLE public.quotes_task
+(
+    "ID" integer,
+    "TIME" integer,
+    "ACCRUEDINT" real,
+    "ASK" real,
+    "ASK_SIZE" integer,
+    "ASK_SIZE_TOTAL" integer,
+    "AVGE_PRCE" real,
+    "BID" real,
+    "BID_SIZE" integer,
+    "BID_SIZE_TOTAL" integer,
+    "BOARDID" text COLLATE pg_catalog."default",
+    "BOARDNAME" character varying COLLATE pg_catalog."default",
+    "BUYBACKDATE" date,
+    "BUYBACKPRICE" real,
+    "CBR_LOMBARD" real,
+    "CBR_PLEDGE" real,
+    "CLOSE" real,
+    "CPN" real,
+    "CPN_DATE" date,
+    "CPN_PERIOD" smallint,
+    "DEAL_ACC" smallint,
+    "FACEVALUE" smallint,
+    "ISIN" character varying COLLATE pg_catalog."default",
+    "ISSUER" character varying COLLATE pg_catalog."default",
+    "ISSUESIZE" integer,
+    "MAT_DATE" date,
+    "MPRICE" real,
+    "MPRICE2" real,
+    "SPREAD" real,
+    "VOL_ACC" integer,
+    "Y2O_ASK" real,
+    "Y2O_BID" real,
+    "YIELD_ASK" real,
+    "YIELD_BID" real
+)
+
+TABLESPACE pg_default;
+
+--Команда назначает владельца таблицы
+
+ALTER TABLE public.quotes_task
+    OWNER to postgres;
+    
+--Команда импортирует данные в созданную таблицу. Выполнять через терминал  SQL Shell,
+--так как в другом способе могут возникнуть проблемы с доступом к файлу
+
+\copy public.quotes_task FROM 'C:/data/quotes_task.csv' DELIMITER ';' CSV HEADER ENCODING 'WIN 1251';
