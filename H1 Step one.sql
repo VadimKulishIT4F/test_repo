f4b07cd3f0f48f5691cf0f815974d23e90717aa8
VadimKulishIT4F <57797583+VadimKulishIT4F@users.noreply.github.com>
2019-11-16 21:08:33 +0300

    Update H1 Step one.sql

--- H1 Step one.sql
+++ H1 Step one.sql
@@ -1,1 +1,34 @@
 
+CREATE TABLE public.bond_description_task
+(
+   "ISIN, RegCode, NRDCode" varchar(12),
+	"FinToolType" text,
+	"SecurityType" text,
+	"SecurityKind" text,
+	"CouponType" text,
+	"RateTypeNameRus_NRD" text,
+	"CouponTypeName_NRD" text,
+	"HaveOffer" boolean,
+	"AmortisedMty" boolean,
+	"MaturityGroup" text,
+	"IsConvertible" boolean,
+	"ISINCode" varchar (12),
+	"Status" text,
+	"HaveDefault" boolean,
+	"GuaranteeType" text,
+	"GuaranteeAmount" text,
+	"BorrowerName" text,
+	"BorrowerOKPO" integer,
+	"BorrowerSector" text,
+	"BorrowerUID" integer,
+	"IssuerName" text,
+	"IssuerName_NRD" text,
+	"IssuerOKPO" integer,
+	"NumGuarantors" smallint,
+	"EndMtyDate" date,
+	CONSTRAINT bond_description_task_pkey PRIMARY KEY ("ISIN, RegCode, NRDCode")
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
