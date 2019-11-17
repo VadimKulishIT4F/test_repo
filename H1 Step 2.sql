8037beb63b2e73753b190d464a5ee85ae0656ec2
VadimKulishIT4F <57797583+VadimKulishIT4F@users.noreply.github.com>
2019-11-17 18:30:42 +0300

    Update H1 Step 2.sql

--- H1 Step 2.sql
+++ H1 Step 2.sql
@@ -9,3 +9,17 @@ add "IssuerName_NRD" text,
 add "IssuerOKPO" integer,
 add "IssuerSector" text;
 
+--  Для проверки добавления столбцов выполним данную команду
+
+SELECT * FROM public.listing_task;
+
+
+--  Данная команда добавляет данные данные из таблицы bond_description_task  в созданные стоблцы из предыдущего пункта
+
+UPDATE public.listing_task
+SET "IssuerName"=public.bond_description_task."IssuerName",
+"IssuerName_NRD"=public.bond_description_task."IssuerName_NRD",
+"IssuerOKPO"=public.bond_description_task."IssuerOKPO",
+"IssuerSector"=public.bond_description_task."BorrowerSector"
+FROM public.bond_description_task
+WHERE listing_task."ISIN"=public.bond_description_task."ISIN, RegCode, NRDCode";
