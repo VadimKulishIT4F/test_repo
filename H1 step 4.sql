604644c4513b1e4ee93f445eec36c211a5223542
VadimKulishIT4F <57797583+VadimKulishIT4F@users.noreply.github.com>
2019-11-18 01:42:40 +0300

    Update H1 step 4.sql

--- H1 step 4.sql
+++ H1 step 4.sql
@@ -1,4 +1,4 @@
---  =((
+-- код возвращает только имена эмитентов и ISIN код бумаг которые торгруются в режиме основной на мосбирже  =((
 select  "IssuerName", "ISIN"
 from public.listing_task 
-where "Section"=' Основной'
+where "Section"=' Основной' or "Section"=' МБ Основной Т+' or "Section"=' МБ Основной Т+'
