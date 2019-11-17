ecb6d2fd54e70aa57ea7915c789b17ced7903e56
VadimKulishIT4F <57797583+VadimKulishIT4F@users.noreply.github.com>
2019-11-17 18:00:24 +0300

    Update H1 Step 2.sql

--- H1 Step 2.sql
+++ H1 Step 2.sql
@@ -1,1 +1,11 @@
+--Команда добавляет новые стоблцы в таблицу listing_task  исходя из информации об эмитентах,
+--которая находится в таблице bond_description_task. (Примечание: новый столбец "IssuerSector" создается несмотря 
+--на то, что в таблице bond_description_task данный столбец отсутствует, т.к. в таблице
+--bond_description_task  столбец BorrowerSector фактически равнозначен новому стоблцу IssuerSector)
+
+ALTER TABLE listing_task  
+add "IssuerName" text,
+add "IssuerName_NRD" text,
+add "IssuerOKPO" integer,
+add "IssuerSector" text;
 
