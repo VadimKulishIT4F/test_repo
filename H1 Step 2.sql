a5e3f60ae6874bd1fcd628ed0eb6c7b31c7f9ad8
VadimKulishIT4F <57797583+VadimKulishIT4F@users.noreply.github.com>
2019-11-17 18:17:54 +0300

    Update H1 Step 2.sql

--- H1 Step 2.sql
+++ H1 Step 2.sql
@@ -3,7 +3,7 @@
 --на то, что в таблице bond_description_task данный столбец отсутствует, т.к. в таблице
 --bond_description_task  столбец BorrowerSector фактически равнозначен новому стоблцу IssuerSector)
 
-ALTER TABLE listing_task  
+ALTER TABLE public.listing_task  
 add "IssuerName" text,
 add "IssuerName_NRD" text,
 add "IssuerOKPO" integer,
